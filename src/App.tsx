2689dde0ccee1b923b0adc5f06b407467dba2312
dieudh-runsystem <dieudh@runsystem.net>
2025-03-17 11:44:42 +0700

    fix(redux): uncomment Redux Provider in App component
    - Enable Redux store connection by uncommenting Provider wrapper
    - Ensure Redux state management is properly connected to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
 import "./App.css";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
 import "./assets/css/bootstrap.css";
 import "./assets/css/color.css";
 import "./assets/css/fontawesome.css";
 import "./assets/css/plugins.css";
-import "./assets/css/responsive.css";
 import "./assets/css/style.css";
+import "jquery";
+// import "./assets/js/jqueryCustome";
+import AppRoutes from "./routes/AppRoutes";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppRoutes />
+      </div>
+    </Provider>
   );
 }
 
